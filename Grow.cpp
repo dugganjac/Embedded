9c955baddd400107d09ede2ead1d1582202cb2c5
Cyelliott <cyelliott0@gmail.com>
2020-01-23 17:26:47 -0500

    Add files via upload

---
+++ Grow.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+# include <string>
+using namespace std;
+
+ int Grow(int new_capacity); {
+double *nv = new double[new_capacity];
+
+    for(i = 0;i <=size; i++) {
+        nv[i] = v[i]
+    }
+
+delete[]
+v;
+v = nv;
+
+cout<< "Vector grown" << "\n";
+cout<< "Previous capacity: " << capacity << " elements" << "\n";
+cout << "New capacity: " << newcapacity << " elements" << "\n";
+
+ capacity = new_capacity;
+}
